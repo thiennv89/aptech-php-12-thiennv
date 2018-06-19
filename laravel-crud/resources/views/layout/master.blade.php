a6c8bde57f708aa3cc20edf4d8e4dcb2d9ad0416
thiennv89 <vietthien211@gmail.com>
2018-06-19 08:32:24 +0700

    softDeleting

--- laravel-crud/resources/views/layout/master.blade.php
+++ laravel-crud/resources/views/layout/master.blade.php
@@ -11,6 +11,16 @@
     <div class="container">
         <div class="d-flex flex-row">
             <div class="col-12">
+                <div class="d-flex flex-row align-items-center">
+                    <div class="col-1">
+                        <form action="{{route('home')}}" method="get">
+                            <button class="btn btn-primary">Home</button>
+                        </form>
+                    </div>
+                    <div class="col-11">
+                        <h1 class="text-center">NGUYEN VIET THIEN - APTECH PHP 12</h1>
+                    </div>
+                </div>
                 @section('content')
 
                 @show
